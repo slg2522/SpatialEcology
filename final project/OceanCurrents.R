67f4fb804aa5c8d863b2d7bde3aae698898d4ed6
slg2522 <77139318+slg2522@users.noreply.github.com>
2022-10-18 23:28:46 -0400

    temperature at depth grid plots

--- final project/OceanCurrents.R
+++ final project/OceanCurrents.R
@@ -95,71 +95,72 @@ mapTheme <- rasterTheme(region = rev(brewer.pal(11, "RdBu")))
 cutpts <- c(-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17)
 #add below for specification: at=cutpts, cuts=11,
 plt1 <- levelplot(subset(tmp_raster.crop3, 1), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                 main="TEMP1")
+                 main="Temperature Depth 1")
 plt1
-plt3 <- levelplot(subset(tmp_raster.crop3, 3), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                  main="TEMP3")
-plt3
 plt5 <- levelplot(subset(tmp_raster.crop3, 5), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                  main="TEMP5")
+                  main="Temperature Depth 5")
 plt5
 plt7 <- levelplot(subset(tmp_raster.crop3, 7), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                  main="TEMP7")
+                  main="Temperature Depth 7")
 plt7
 plt9 <- levelplot(subset(tmp_raster.crop3, 9), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                  main="TEMP9")
+                  main="Temperature Depth 9")
 plt9
 plt11 <- levelplot(subset(tmp_raster.crop3, 11), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP11")
+                   main="Temperature Depth 11")
 plt11
 plt13 <- levelplot(subset(tmp_raster.crop3, 13), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP13")
+                   main="Temperature Depth 13")
 plt13
 plt15 <- levelplot(subset(tmp_raster.crop3, 15), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP15")
+                   main="Temperature Depth 15")
 plt15
 plt17 <- levelplot(subset(tmp_raster.crop3, 17), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP17")
+                   main="Temperature Depth 17")
 plt17
 plt19 <- levelplot(subset(tmp_raster.crop3, 19), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP19")
+                   main="Temperature Depth 19")
 plt19
 plt21 <- levelplot(subset(tmp_raster.crop3, 21), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP21")
+                   main="Temperature Depth 21")
 plt21
 plt23 <- levelplot(subset(tmp_raster.crop3, 23), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP23")
+                   main="Temperature Depth 23")
 plt23
 plt25 <- levelplot(subset(tmp_raster.crop3, 25), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP25")
+                   main="Temperature Depth 25")
 plt25
 plt27 <- levelplot(subset(tmp_raster.crop3, 27), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP27")
+                   main="Temperature Depth 27")
 plt27
 plt29 <- levelplot(subset(tmp_raster.crop3, 29), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP29")
+                   main="Temperature Depth 29")
 plt29
 plt31 <- levelplot(subset(tmp_raster.crop3, 31), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP31")
+                   main="Temperature Depth 31")
 plt31
 plt33 <- levelplot(subset(tmp_raster.crop3, 33), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP33")
+                   main="Temperature Depth 33")
 plt33
 plt35 <- levelplot(subset(tmp_raster.crop3, 35), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP35")
+                   main="Temperature Depth 35")
 plt35
 plt37 <- levelplot(subset(tmp_raster.crop3, 37), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP37")
+                   main="Temperature Depth 37")
 plt37
 plt39 <- levelplot(subset(tmp_raster.crop3, 39), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP39")
+                   main="Temperature Depth 39")
 plt39
 plt41 <- levelplot(subset(tmp_raster.crop3, 41), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP41")
+                   main="Temperature Depth 41")
 plt41
-plt43 <- levelplot(subset(tmp_raster.crop3, 43), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
-                   main="TEMP43")
-plt43
+plt44 <- levelplot(subset(tmp_raster.crop3, 44), margin = F,  at=cutpts, cuts=20, pretty=TRUE, par.settings = mapTheme,
+                   main="Temperature Depth 44")
+plt44
+
+#grid of all the plots incrementally
+library(gridExtra)
+grid.arrange(plt1, plt5, plt7, plt11, plt15, plt19, plt23, plt27, plt31, plt35, plt39, plt44, ncol=4, nrow=3)
 
 
 #HU: ocean depth at U points
