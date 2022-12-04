9b6566c8c058bf3c050d14982621b615f4dd2a5f
slg2522 <77139318+slg2522@users.noreply.github.com>
2022-12-04 11:16:39 -0500

    homework 7 question 3 complete

--- Homework7/homework7.R
+++ Homework7/homework7.R
@@ -165,6 +165,12 @@ plot(log10(step),log10(freq),xlab="Log(step length)",
      ylab="Log(frequency)",type="b",las=1)
 dev.off()
 
+#does step lengths distribution resemble a  Levy flight model?
+#estimate the Levy parameter mu from a linear model of 
+#log frequency versus log step length.
+model<-lm(log10(freq)~log10(step))
+summary(model)
+
 #Turtle2 Plotting
 # plot the step length histogram on log scale (with step + 1 to avoid infinites)
 # Change plot filename to appropriate ID number.
@@ -182,6 +188,12 @@ png(filename = "Turtle2StepLengthDistribution.png",
 plot(log10(step),log10(freq),xlab="Log(step length)", 
      ylab="Log(frequency)",type="b",las=1)
 dev.off()
+
+#does step lengths distribution resemble a  Levy flight model?
+#estimate the Levy parameter mu from a linear model of 
+#log frequency versus log step length.
+model<-lm(log10(freq)~log10(step))
+summary(model)
 
 #Turtle6 Plotting
 # plot the step length histogram on log scale (with step + 1 to avoid infinites)
@@ -201,6 +213,12 @@ plot(log10(step),log10(freq),xlab="Log(step length)",
      ylab="Log(frequency)",type="b",las=1)
 dev.off()
 
+#does step lengths distribution resemble a  Levy flight model?
+#estimate the Levy parameter mu from a linear model of 
+#log frequency versus log step length.
+model<-lm(log10(freq)~log10(step))
+summary(model)
+
 #Turtle7 Plotting
 # plot the step length histogram on log scale (with step + 1 to avoid infinites)
 # Change plot filename to appropriate ID number.
@@ -218,6 +236,12 @@ png(filename = "Turtle7StepLengthDistribution.png",
 plot(log10(step),log10(freq),xlab="Log(step length)", 
      ylab="Log(frequency)",type="b",las=1)
 dev.off()
+
+#does step lengths distribution resemble a  Levy flight model?
+#estimate the Levy parameter mu from a linear model of 
+#log frequency versus log step length.
+model<-lm(log10(freq)~log10(step))
+summary(model)
 
 #Turtle8 Plotting
 # plot the step length histogram on log scale (with step + 1 to avoid infinites)
@@ -237,12 +261,136 @@ plot(log10(step),log10(freq),xlab="Log(step length)",
      ylab="Log(frequency)",type="b",las=1)
 dev.off()
 
+#does step lengths distribution resemble a  Levy flight model?
+#estimate the Levy parameter mu from a linear model of 
+#log frequency versus log step length.
+model<-lm(log10(freq)~log10(step))
+summary(model)
+
 
 ##3. For each turtle ID, does the distribution of step lengths fit a Lévy
 ##flight model distribution?
 
+#Calculations are completed in-line above in question 2 with the following
+#format:
+#model<-lm(log10(freq)~log10(step))
+#summary(model)
+
+#Turtle1
+#Call:
+#  lm(formula = log10(freq) ~ log10(step))
+#
+#Residuals:
+#  Min       1Q   Median       3Q      Max 
+#-0.78760 -0.49222  0.08548  0.51424  0.56890 
+#
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)   
+#(Intercept)   4.1558     1.0308   4.031  0.00378 **
+#  log10(step)  -1.8255     0.6124  -2.981  0.01758 * 
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+#Residual standard error: 0.5849 on 8 degrees of freedom
+#Multiple R-squared:  0.5262,	Adjusted R-squared:  0.467 
+#F-statistic: 8.886 on 1 and 8 DF,  p-value: 0.01758
+
+# For turtle1, the log10(step) coefficient is -1.8255. 
+# This gives a Mu value of 1.8255, which is consistent with a Levy flight
+#(absolute value between 1 and 3).
+
+#Turtle2
+#Call:
+#  lm(formula = log10(freq) ~ log10(step))
+#
+#Residuals:
+#  Min      1Q  Median      3Q     Max 
+#-0.6845 -0.2934  0.1434  0.2298  0.4992 
+#
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)   1.9957     0.4400   4.535 0.000394 ***
+#  log10(step)  -0.6890     0.2766  -2.491 0.024957 *  
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+#Residual standard error: 0.3824 on 15 degrees of freedom
+#Multiple R-squared:  0.2926,	Adjusted R-squared:  0.2454 
+#F-statistic: 6.204 on 1 and 15 DF,  p-value: 0.02496
+
+# For turtle2, the log10(step) coefficient is -0.6890. 
+# This gives a Mu value of 0.6890, which is inconsistent with a Levy flight
+#(absolute value between 1 and 3).
+
+#Turtle6
+#Call:
+#  lm(formula = log10(freq) ~ log10(step))
+#
+#Residuals:
+#  Min       1Q   Median       3Q      Max 
+#-0.50803 -0.18610  0.07658  0.24985  0.37956 
+#
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)    
+#(Intercept)   3.7027     0.4540   8.156 9.94e-06 ***
+#  log10(step)  -1.6626     0.3117  -5.335 0.000331 ***
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+#Residual standard error: 0.3393 on 10 degrees of freedom
+#Multiple R-squared:   0.74,	Adjusted R-squared:  0.714 
+#F-statistic: 28.46 on 1 and 10 DF,  p-value: 0.0003308
+
+# For turtle6, the log10(step) coefficient is -1.6626. 
+# This gives a Mu value of 1.6626, which is consistent with a Levy flight
+#(absolute value between 1 and 3).
+
+#Turtle7
+#Call:
+#  lm(formula = log10(freq) ~ log10(step))
+#
+#Residuals:
+#  Min       1Q   Median       3Q      Max 
+#-0.43138 -0.19072 -0.02458  0.17492  0.33692 
+#
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)  
+#(Intercept)   1.6463     0.5073   3.245   0.0141 *
+#  log10(step)  -0.4367     0.3085  -1.416   0.1998  
+#---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+#Residual standard error: 0.2726 on 7 degrees of freedom
+#Multiple R-squared:  0.2226,	Adjusted R-squared:  0.1115 
+#F-statistic: 2.004 on 1 and 7 DF,  p-value: 0.1998
+
+# For turtle7, the log10(step) coefficient is -0.4367. 
+# This gives a Mu value of 0.4367, which is inconsistent with a Levy flight
+#(absolute value between 1 and 3).
 
 
+#Turtle8
+#Call:
+#  lm(formula = log10(freq) ~ log10(step))
+#
+#Residuals:
+#  Min       1Q   Median       3Q      Max 
+#-0.51507 -0.19213 -0.07224  0.22607  0.66044 
+#
+#Coefficients:
+#  Estimate Std. Error t value Pr(>|t|)   
+#(Intercept)   3.5136     0.7987   4.399  0.00316 **
+#  log10(step)  -1.2582     0.4857  -2.590  0.03593 * 
+#  ---
+#  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+#Residual standard error: 0.4292 on 7 degrees of freedom
+#Multiple R-squared:  0.4894,	Adjusted R-squared:  0.4165 
+#F-statistic:  6.71 on 1 and 7 DF,  p-value: 0.03593
+
+# For turtle8, the log10(step) coefficient is -1.2582. 
+# This gives a Mu value of 1.2582, which is consistent with a Levy flight
+#(absolute value between 1 and 3).
 
 
   
